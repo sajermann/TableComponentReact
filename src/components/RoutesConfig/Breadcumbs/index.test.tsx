800ee7fbd188393439c2d854dfabd5e61acb769c
Sajermann <sajermannbruno@gmail.com>
2025-10-02 20:21:00 -0300

    add some tests

--- src/components/RoutesConfig/Breadcumbs/index.test.tsx
+++ src/components/RoutesConfig/Breadcumbs/index.test.tsx
@@ -1,80 +1,68 @@
-/**
- * @vitest-environment jsdom
- */
-import { render } from "@testing-library/react";
-import { afterEach, describe, expect, it, vi } from "vitest";
-import { InjectorProviders } from "~/components/InjectorProviders";
-import * as useBreadcrumbsMock from "~/hooks/useBreadcrumbs";
-
+import * as reactRouter from "@tanstack/react-router";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { useBreadcrumbs } from "~/hooks/useBreadcrumbs";
 import { Breadcrumbs } from ".";
 
-describe("Components/Breadcrumbs", () => {
-  afterEach(() => {
-    vi.clearAllMocks();
-    vi.resetAllMocks();
+vi.mock("~/hooks", () => ({
+  useTranslation: () => ({
+    translate: (label: string) => label.toUpperCase(), // Simples transformação para visualização
+  }),
+}));
+vi.mock("~/hooks/useBreadcrumbs");
+vi.mock("@tanstack/react-router", async () => {
+  return {
+    // Mockando Link como uma função para sobrescrever nos testes
+    Link: vi.fn(),
+    useLocation: vi.fn(),
+  };
+});
 
-    vi.mock("react-router", async () => {
-      const mod = await vi.importActual<any>("react-router");
-      return {
-        ...mod,
-        useLocation: () => ({
+describe("components/routesConfig/Breadcrumbs", () => {
+  it("render breadcrumbs correctly", () => {
+    vi.mocked(reactRouter.Link).mockImplementation((props: any) => (
+      <a {...props} />
+    ));
+    vi.mocked(reactRouter.useLocation).mockImplementation(
+      () =>
+        ({
+          pathname: "/about",
+        }) as any
+    );
+
+    vi.mocked(useBreadcrumbs).mockImplementation(() => ({
+      breadcrumbs: [
+        { label: "home", link: "/" },
+        { label: "about", link: "/about" },
+        { label: "contact", link: null },
+      ],
+      setBreadcrumbs: vi.fn(),
+    }));
+
+    render(<Breadcrumbs />);
+    expect(screen.getByText("HOME")).toBeInTheDocument();
+    expect(screen.getByText("ABOUT")).toBeInTheDocument();
+    expect(screen.getByText("CONTACT")).toBeInTheDocument();
+  });
+
+  it("dont render if breadcrumbs is empty", () => {
+    vi.mocked(useBreadcrumbs).mockImplementation(() => ({
+      breadcrumbs: [],
+      setBreadcrumbs: vi.fn(),
+    }));
+    render(<Breadcrumbs />);
+    expect(screen.queryByText("HOME")).not.toBeInTheDocument();
+  });
+
+  it("dont render if path is root", () => {
+    vi.mocked(reactRouter.useLocation).mockImplementation(
+      () =>
+        ({
           pathname: "/",
-        }),
-      };
-    });
-  });
-
-  it(`must render null`, async () => {
-    vi.spyOn(useBreadcrumbsMock, "useBreadcrumbs").mockImplementation(() => ({
-      breadcrumbs: [
-        {
-          label: `Test`,
-          link: ``,
-        },
-      ],
-      setBreadcrumbs: vi.fn(),
-    }));
-    // vi.spyOn(useLocationMock, "useLocation").mockImplementation(
-    //   () =>
-    //     ({
-    //       pathname: "/",
-    //     }) as any
-    // );
-    const { queryAllByText } = render(
-      <InjectorProviders>
-        <Breadcrumbs />
-      </InjectorProviders>
+        }) as any
     );
-    const result = queryAllByText("Test");
-    expect(result.length).toBe(0);
-  });
 
-  it(`must render null`, async () => {
-    vi.spyOn(useBreadcrumbsMock, "useBreadcrumbs").mockImplementation(() => ({
-      breadcrumbs: [
-        {
-          label: `Test`,
-          link: `link-test`,
-        },
-        {
-          label: `Test 1`,
-          link: `link-test-1`,
-        },
-      ],
-      setBreadcrumbs: vi.fn(),
-    }));
-    // vi.spyOn(useLocationMock, "useLocation").mockImplementation(
-    //   () =>
-    //     ({
-    //       pathname: "test-route",
-    //     }) as any
-    // );
-    const { queryAllByText } = render(
-      <InjectorProviders>
-        <Breadcrumbs />
-      </InjectorProviders>
-    );
-    const result = queryAllByText("Test");
-    expect(result.length).toBe(1);
+    render(<Breadcrumbs />);
+    expect(screen.queryByText("HOME")).not.toBeInTheDocument();
   });
 });
